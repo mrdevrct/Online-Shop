ebcae2b8333fca7811dc4bbada3fa3cdfe0133c8
IamNot <rainbowcoffeepubg@gmail.com>
2023-06-11 21:15:55 +0330

    add database main

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 class Users(models.Model):
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    username = models.CharField(max_length=255, default='N/A')
+    name_lastname = models.CharField(max_length=255, default='N/A')
+    password = models.CharField(max_length=255, default='N/A')
+    email = models.EmailField(max_length=255, default='N/A')
+    phone_number = models.CharField(max_length=255, default='N/A')
+    city = models.CharField(max_length=255, default='N/A')
+    address = models.CharField(max_length=255, default='N/A')
+    national_code = models.CharField(max_length=255, default='N/A')

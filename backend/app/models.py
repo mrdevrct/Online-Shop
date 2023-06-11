9c935dabc516049177d681c52933d1195d73f481
IamNot <rainbowcoffeepubg@gmail.com>
2023-06-11 21:03:36 +0330

    models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 class Users(models.Model):
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    name_lastname = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    national_code = models.CharField(max_length=100)
